14dc570b26fa15abc1b6c326ac8bb1103df5ef82
annapmarin <annapratsmarin@gmail.com>
2023-04-02 19:39:38 +0200

    calculator logic and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,158 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/index.css";
+import iconArrow from "./assets/images/icon-arrow.svg";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [day, setDay] = useState("--");
+  const [month, setMonth] = useState("--");
+  const [year, setYear] = useState("--");
+  const [calcYear, setCalcYear] = useState("--");
+  const [calcMonth, setCalcMonth] = useState("--");
+  const [calcDay, setCalcDay] = useState("--");
+  //const [errorInvalidDate, setErrorInvalidDate] = useState()
+
+  const date = new Date();
+  const actualYear = date.getFullYear();
+  const actualMonth = date.getMonth() + 1;
+  const actualDay = date.getDate();
+
+  const handleDay = (event) => {
+    if (Number(event.target.value) > 0 && Number(event.target.value) < 32) {
+      setDay(Number(event.target.value));
+    }
+  };
+
+  const handleMonth = (event) => {
+    if (Number(event.target.value) > 0 && Number(event.target.value) < 13) {
+      setMonth(Number(event.target.value));
+    }
+  };
+
+  const handleYear = (event) => {
+    if (Number(event.target.value) > 1900 && Number(event.target.value) <= actualYear) {
+      setYear(Number(event.target.value));
+    }
+
+  };
+
+  const calculator = () => {
+    // calc year
+    if (actualMonth < month) {
+      setCalcYear(actualYear - year - 1);
+    } else if (actualMonth > month) {
+      setCalcYear(actualYear - year);
+    } else if (actualMonth === month) {
+      if (actualDay < day) {
+        setCalcYear(actualYear - year - 1);
+      } else if (actualDay >= day) {
+        setCalcYear(actualYear - year);
+      }
+    }
+    // calc month
+    if (actualMonth < month) {
+      switch (month) {
+        case 1:
+          setCalcMonth(0);
+          break;
+        case 2:
+          setCalcMonth(11 - actualMonth);
+          break;
+        case 3:
+          setCalcMonth(10 + actualMonth);
+          break;
+        case 4:
+          setCalcMonth(9 + actualMonth - 2);
+          break;
+        case 5:
+          setCalcMonth(8 + actualMonth - 2);
+          break;
+        case 6:
+          setCalcMonth(7 + actualMonth - 2);
+          break;
+        case 7:
+          setCalcMonth(6 + actualMonth - 2);
+          break;
+        case 8:
+          setCalcMonth(5 + actualMonth - 2);
+          break;
+        case 9:
+          setCalcMonth(4 + actualMonth - 2);
+          break;
+        case 10:
+          setCalcMonth(3 + actualMonth - 2);
+          break;
+        case 11:
+          setCalcMonth(2 + actualMonth - 2);
+          break;
+        case 12:
+          setCalcMonth(1 + actualMonth - 2);
+          break;
+      }
+    } else if (actualMonth > month) {
+      setCalcMonth(actualMonth - month - 1);
+    } else if (actualMonth === month) {
+      setCalcMonth("--");
+    }
+    // calc day
+    if (actualDay < day) {
+      if (
+        month === 1 ||
+        month === 3 ||
+        month === 5 ||
+        month === 7 ||
+        month === 8 ||
+        month === 10 ||
+        month === 12
+      ) {
+        setCalcDay(31 - day + actualDay);
+      } else if (month === 2 && year % 4 === 0) {
+        setCalcDay(29 - day + actualDay);
+      } else if (month === 2 && year % 4 !== 0) {
+        setCalcDay(28 - day + actualDay);
+      } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+        setCalcDay(30 - day + actualDay);
+      }
+    } else if (actualDay > day) {
+      setCalcDay(actualDay - day);
+    } else if (actualDay === day) {
+      setCalcDay("--");
+    }
+  };
 
   return (
-    <div className="App">
+    <main className="container">
+      <section>
+        <div className="container__labels">
+          <p>day</p>
+          <p>month</p>
+          <p>year</p>
+        </div>
+        <div className="container__inputs">
+          <input onBlur={handleDay} placeholder="DD" />
+          <input onBlur={handleMonth} placeholder="MM" />
+          <input onBlur={handleYear} placeholder="YYYY" />
+        </div>
+        <span className="error-invalid-date">{}</span>
+      </section>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <hr className="container__separator" />
+        <button onClick={calculator} className="container__separator__button">
+          <img src={iconArrow} className="container__separator__image" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+      <section className="container__data">
+        <p>
+          <span>{calcYear}</span> years
+        </p>
+        <p>
+          <span>{calcMonth}</span> {calcMonth === 1 ? "month" : "months"}
+        </p>
+        <p>
+          <span>{calcDay}</span> {calcDay === 1 ? "day" : "days"}
+        </p>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
